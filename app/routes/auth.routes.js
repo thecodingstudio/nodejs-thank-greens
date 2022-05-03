85bf109054a6cc7bf6f0931172551f0800be048c
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-05-03 16:40:55 +0530

    Update auth.routes.js

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
+const is_auth = require('../middlewares/is-auth');
 
 router.use(cors());
 
@@ -13,6 +14,8 @@ router.post('/refresh-token', authController.refreshToken);
 
 router.post('/forgot-password', authController.forgotPassword);
 
-// router.post('/change-password', authController.changePassword);
+router.post('/change-password', is_auth, authController.changePassword);
+
+router.get('/logout', is_auth, authController.Logout);
 
 module.exports = router;
