4bb83d5f06e3ef5cb726d7b148f7e4c7f369d7ef
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-05-17 09:22:48 +0530

    Fix search api

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -426,9 +426,12 @@ exports.Sort = async (req, res, next) => {
 }
 
 exports.Search = async (req, res, next) => {
-    const term = req.query.term;
-    if (term.length < 4) {
-        return res.status(400).json({ ErrorMessage: 'Term length must be more than 3 letters', status: 0 });
+    try {
+        const term = req.query.term;
+    if(term){
+        if (term.length < 4) {
+            return res.status(400).json({ ErrorMessage: 'Term length must be more than 3 letters', status: 0 });
+        }
     }
     try {
         const category = await Category.findAll({
@@ -490,5 +493,8 @@ exports.Search = async (req, res, next) => {
     catch (error) {
         console.log(error);
         return res.status(400).json({ ErrorMessage: 'Failed to search!', status: 0 });
+    }
+    } catch (error) {
+        next(error);
     }
 }
